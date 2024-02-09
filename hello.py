ad80345f5d4342df6bc12ca4961fb03a6443f4b5
Michael Maggs <runawayserver@infinitelyloopee.dev>
2024-02-09 12:44:59 -0800

    added product page endpoint

--- hello.py
+++ hello.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/products")
+def products():
+    return "<p>Product Page</p>"
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
 if __name__ == '__main__':
     # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
